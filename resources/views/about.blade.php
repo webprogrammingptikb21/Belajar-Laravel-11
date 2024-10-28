994674ee1865d8f1c2079efdc2096579ce4729cd
Alwiii <210209501011@student.unm.ac.id>
2024-10-28 21:14:59 +0800

    menambahkan file laravel 11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman About</h3>
+    <p>Nama : {{ $nama }}</p>
+</x-layout>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    @if ($posts && $posts->count())
+        @foreach ($posts as $post)
+            <article class="py-8 max-w-screen-md border-b border-gray-300">
+                <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+                    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+                </a>
+                <div class="text-base text-gray-500 ">
+                    <a class="hover:underline" href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> |
+                    {{ $post->created_at->diffForHumans() }}
+                </div>
+                <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
+                <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More
+                    &raquo;</a>
+            </article>
+        @endforeach
+    @else
+        <p class="text-center">No posts available.</p>
+    @endif
+</x-layout>
