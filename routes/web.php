f96a79395a8a2e103c944ae582076233d3a3f66d
Alwiii <210209501011@student.unm.ac.id>
2024-11-20 21:22:48 +0800

    menambahkan kategori

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -20,8 +21,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
-    return view('posts', ['title' => 'Article by: ' . $user->name, 'posts' => $user->posts]);
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->posts)   .   ' Article by: '  . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Articles in: '  . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
